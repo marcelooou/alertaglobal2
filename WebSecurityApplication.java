57a5e56db31c7ea4ed7640d8762dc78855e89eaa
marcelooou <marcelosiqueirabonfim@gmail.com>
2025-06-03 20:36:48 -0300

    22

--- WebSecurityApplication.java
+++ WebSecurityApplication.java
@@ -1,9 +1,10 @@
 package com.fiap.alertachuva;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication   // sem scanBasePackages, pois está no pacote raiz
+@SpringBootApplication
+@ComponentScan(basePackages = "com.fiap.alertachuva")
 public class WebSecurityApplication {
     public static void main(String[] args) {
         SpringApplication.run(WebSecurityApplication.class, args);
